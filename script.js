1d21cef9332338c1663064a2a7eb3dd444f16483
Crooks2k <ciberronaldo@hotmail.com>
2022-11-26 18:28:38 -0500

    Version 2.0 Extended

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function numeros(valor){
 function operacion(){
     const opera = document.getElementById("resultado").value //seleccionamos el id resultado
     if (opera == 0){ //Este codigo se ejecutara y saldra un mensaje de error si el usuario no digita nada
-        document.getElementById("resultado").value = "E"
+        document.getElementById("resultado").value = "Syntax Error"
     }
     else{ //este codigo se ejecutara si se introduce alguna operacion
         document.getElementById("resultado").value = eval(opera)
@@ -24,3 +24,6 @@ function clearspace(){
     const clearspacedata = document.getElementById("resultado").value
     document.getElementById("resultado").value = clearspacedata.substr(0, clearspacedata.length -1);
 }
+
+//valor de pi
+const pi = 3.14159265359
