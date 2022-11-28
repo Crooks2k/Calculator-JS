772b08c9aac34c1f865769bfeb2d4c1d8e9191c0
Crooks2k <ciberronaldo@hotmail.com>
2022-11-28 16:09:04 -0500

    Version 3.0 Security Fix

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ function numeros(valor){
 }
 
 //funcion para dar el resultado =
+//Función para procesar el resultado con un regex
 function operacion(){
     const opera = document.getElementById("resultado").value //seleccionamos el id resultado
-    if (opera == 0){ //Este codigo se ejecutara y saldra un mensaje de error si el usuario no digita nada
+    const regex = /[`,!,@,#,$,^,&,{},||,",;,_,=,b,d-f,j,k,ñ,p-r,u-z ]/ig; //agregar todos los signos que no quiero que se usen en el input
+    Valueok = regex.test(opera) 
+    //Si la evaluación del regex sobre el valor del input es True quiere decir que hay codigo malicioso entonces arrojara
+    //error, pero si no hay codigo malicioso se ejecutara el else y el valor del input sera el eval sobre Valueok
+    if(opera == 0){ //Este codigo se ejecutara y saldra un mensaje de error si el usuario no digita nada
         document.getElementById("resultado").value = "Syntax Error"
     }
-    else{ //este codigo se ejecutara si se introduce alguna operacion
+    else if(Valueok == true){
+        document.getElementById("resultado").value = "Syntax Error"
+    }
+    else if(Valueok == false){
         document.getElementById("resultado").value = eval(opera)
     }
 }
